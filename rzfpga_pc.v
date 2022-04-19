f3975b358edb45f1ba81e85c231d8e1433b45c35
conorm <mjoconnor110@gmail.com>
2022-04-18 19:23:20 -0500

    vga demo working in background bc its cool

--- rzfpga_pc.v
+++ rzfpga_pc.v
@@ -2,7 +2,14 @@ module rzfpga_pc (
 	output wire led1,
 	output wire led2,
 	output wire led3,
-	output wire led4
+	output wire led4,
+	
+	input clk50,
+	output pix0,
+	output pix1,
+	output pix2,
+	output hsyncout,
+	output vsyncout
 );
 
 wire [15:0] outbuf;
@@ -27,9 +34,41 @@ ALUn2t ALU_instance_main (
 	.out(outbuf),
 	.zr(zrbuf),
 	.ng(ngbuf)
-	
 );
 
+wire inDisplayArea;
+wire [9:0] CounterX;
+reg [2:0] pixel;
+
+wire clk_25;
+
+hvsync_generator hvsync(
+.clk(clk_25),
+.vga_h_sync(hsyncout),
+.vga_v_sync(vsyncout),
+.CounterX(CounterX),
+//.CounterY(CounterY),
+.inDisplayArea(inDisplayArea)
+);
+
+// wrapper for clock divider
+clock_divider wrapper (
+  .clk50(clk50),
+  .clk_25(clk_25)
+);
+
+
+always @(posedge clk_25)
+begin
+if (inDisplayArea)
+  pixel <= CounterX[9:6];
+else // if it's not to display, go dark
+  pixel <= 3'b000;
+end
+
+assign pix0 = pixel[0];
+assign pix1 = pixel[1];
+assign pix2 = pixel[2];
 
 
 endmodule
