6e8cc6d25e5b8cb3f6c6f29cca4522b090ac3108
conorm <mjoconnor110@gmail.com>
2022-06-07 19:24:14 -0500

    testing

--- rzfpga_pc.v
+++ rzfpga_pc.v
@@ -60,24 +60,32 @@ module rzfpga_pc (
 	output vsyncout
 );
 
-
+wire [15:0] data_in_bus;
 wire [15:0] data_out_bus;
 wire [14:0] data_address_bus;
 wire [14:0] instruction_address_bus;
-assign led1 = data_address_bus[0];
-assign led2 = data_address_bus[1];
-assign led3 = data_address_bus[2];
-assign led4 = data_address_bus[3];
+
+assign led4 = 1'b1;
+assign led3 = 1'b1;
+assign led2 = 1'b1;
+assign led1 = ~(instruction_address_bus[14] & instruction_address_bus[13] & instruction_address_bus[12] & instruction_address_bus[11] & instruction_address_bus[10] & instruction_address_bus[4]);
 wire we;
 hack_cpu cpu (
 	.clk(clk50),
-	.instr_bus(16'b0000000000000001),
-	.data_in_bus(16'b000000000000), // eventually needs to be ram out
-	.reset(1'b0), // set to a button after debuging
+	.instr_bus(16'b0000000000001010),
+	.data_in_bus(data_in_bus), // eventually needs to be ram out
+	.reset(~key0), // set to a button after debuging
 	.data_out_bus(data_out_bus),
 	.write_enable(we),
 	.data_address_bus(data_address_bus),
 	.instruction_address_bus(instruction_address_bus)
 );
-
+ 
+bram_ram_512 main_ram (
+	.in(data_out_bus),
+	.address(data_address_bus),
+	.clk(clk),
+	.load(we),
+	.out(data_in_bus)
+);
 endmodule
