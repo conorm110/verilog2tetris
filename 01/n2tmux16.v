1fdf7bd92aa250f0100bab6e66a1e44612990caa
conorm <mjoconnor110@gmail.com>
2022-08-03 23:43:18 -0500

    Clean Up!

--- 01/n2tmux16.v
+++ 01/n2tmux16.v
@@ -21,21 +21,21 @@ module n2tmux16(
     output [15:0] out
     );
 
-n2tmux mux16_a(.a(a[0]),.b(b[0]),.sel(sel),.out(out[0]));
-n2tmux mux16_b(.a(a[1]),.b(b[1]),.sel(sel),.out(out[1]));
-n2tmux mux16_c(.a(a[2]),.b(b[2]),.sel(sel),.out(out[2]));
-n2tmux mux16_d(.a(a[3]),.b(b[3]),.sel(sel),.out(out[3]));
-n2tmux mux16_e(.a(a[4]),.b(b[4]),.sel(sel),.out(out[4]));
-n2tmux mux16_f(.a(a[5]),.b(b[5]),.sel(sel),.out(out[5]));
-n2tmux mux16_g(.a(a[6]),.b(b[6]),.sel(sel),.out(out[6]));
-n2tmux mux16_h(.a(a[7]),.b(b[7]),.sel(sel),.out(out[7]));
-n2tmux mux16_i(.a(a[8]),.b(b[8]),.sel(sel),.out(out[8]));
-n2tmux mux16_j(.a(a[9]),.b(b[9]),.sel(sel),.out(out[9]));
-n2tmux mux16_k(.a(a[10]),.b(b[10]),.sel(sel),.out(out[10]));
-n2tmux mux16_l(.a(a[11]),.b(b[11]),.sel(sel),.out(out[11]));
-n2tmux mux16_m(.a(a[12]),.b(b[12]),.sel(sel),.out(out[12]));
-n2tmux mux16_n(.a(a[13]),.b(b[13]),.sel(sel),.out(out[13]));
-n2tmux mux16_o(.a(a[14]),.b(b[14]),.sel(sel),.out(out[14]));
-n2tmux mux16_p(.a(a[15]),.b(b[15]),.sel(sel),.out(out[15]));
+n2tmux mux_a(.a(a[0]),.b(b[0]),.sel(sel),.out(out[0]));
+n2tmux mux_b(.a(a[1]),.b(b[1]),.sel(sel),.out(out[1]));
+n2tmux mux_c(.a(a[2]),.b(b[2]),.sel(sel),.out(out[2]));
+n2tmux mux_d(.a(a[3]),.b(b[3]),.sel(sel),.out(out[3]));
+n2tmux mux_e(.a(a[4]),.b(b[4]),.sel(sel),.out(out[4]));
+n2tmux mux_f(.a(a[5]),.b(b[5]),.sel(sel),.out(out[5]));
+n2tmux mux_g(.a(a[6]),.b(b[6]),.sel(sel),.out(out[6]));
+n2tmux mux_h(.a(a[7]),.b(b[7]),.sel(sel),.out(out[7]));
+n2tmux mux_i(.a(a[8]),.b(b[8]),.sel(sel),.out(out[8]));
+n2tmux mux_j(.a(a[9]),.b(b[9]),.sel(sel),.out(out[9]));
+n2tmux mux_k(.a(a[10]),.b(b[10]),.sel(sel),.out(out[10]));
+n2tmux mux_l(.a(a[11]),.b(b[11]),.sel(sel),.out(out[11]));
+n2tmux mux_m(.a(a[12]),.b(b[12]),.sel(sel),.out(out[12]));
+n2tmux mux_n(.a(a[13]),.b(b[13]),.sel(sel),.out(out[13]));
+n2tmux mux_o(.a(a[14]),.b(b[14]),.sel(sel),.out(out[14]));
+n2tmux mux_p(.a(a[15]),.b(b[15]),.sel(sel),.out(out[15]));
 
 endmodule
--- 7 Seg Display/7 Seg Clock Gen IP/clk_7seg.v
+++ 7 Seg Display/7 Seg Clock Gen IP/clk_7seg.v
@@ -4,7 +4,7 @@
 // MODULE: altpll 
 
 // ============================================================
-// File Name: sdram_clk_100MHz.v
+// File Name: clk_7seg.v
 // Megafunction Name(s):
 // 			altpll
 //
@@ -37,7 +37,7 @@
 // synopsys translate_off
 `timescale 1 ps / 1 ps
 // synopsys translate_on
-module sdram_clk_100MHz (
+module clk_7seg (
 	inclk0,
 	c0);
 
@@ -91,14 +91,14 @@ module sdram_clk_100MHz (
 				.vcounderrange ());
 	defparam
 		altpll_component.bandwidth_type = "AUTO",
-		altpll_component.clk0_divide_by = 1,
+		altpll_component.clk0_divide_by = 500,
 		altpll_component.clk0_duty_cycle = 50,
-		altpll_component.clk0_multiply_by = 2,
+		altpll_component.clk0_multiply_by = 1,
 		altpll_component.clk0_phase_shift = "0",
 		altpll_component.compensate_clock = "CLK0",
 		altpll_component.inclk0_input_frequency = 20000,
 		altpll_component.intended_device_family = "Cyclone IV E",
-		altpll_component.lpm_hint = "CBX_MODULE_PREFIX=sdram_clk_100MHz",
+		altpll_component.lpm_hint = "CBX_MODULE_PREFIX=clk_7seg",
 		altpll_component.lpm_type = "altpll",
 		altpll_component.operation_mode = "NORMAL",
 		altpll_component.pll_type = "AUTO",
@@ -167,9 +167,9 @@ endmodule
 // Retrieval info: PRIVATE: CUR_DEDICATED_CLK STRING "c0"
 // Retrieval info: PRIVATE: CUR_FBIN_CLK STRING "c0"
 // Retrieval info: PRIVATE: DEVICE_SPEED_GRADE STRING "Any"
-// Retrieval info: PRIVATE: DIV_FACTOR0 NUMERIC "1"
+// Retrieval info: PRIVATE: DIV_FACTOR0 NUMERIC "500"
 // Retrieval info: PRIVATE: DUTY_CYCLE0 STRING "50.00000000"
-// Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "100.000000"
+// Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "0.100000"
 // Retrieval info: PRIVATE: EXPLICIT_SWITCHOVER_COUNTER STRING "0"
 // Retrieval info: PRIVATE: EXT_FEEDBACK_RADIO STRING "0"
 // Retrieval info: PRIVATE: GLOCKED_COUNTER_EDIT_CHANGED STRING "1"
@@ -195,7 +195,7 @@ endmodule
 // Retrieval info: PRIVATE: MULT_FACTOR0 NUMERIC "1"
 // Retrieval info: PRIVATE: NORMAL_MODE_RADIO STRING "1"
 // Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "100.00000000"
-// Retrieval info: PRIVATE: OUTPUT_FREQ_MODE0 STRING "1"
+// Retrieval info: PRIVATE: OUTPUT_FREQ_MODE0 STRING "0"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_UNIT0 STRING "MHz"
 // Retrieval info: PRIVATE: PHASE_RECONFIG_FEATURE_ENABLED STRING "1"
 // Retrieval info: PRIVATE: PHASE_RECONFIG_INPUTS_CHECK STRING "0"
@@ -212,7 +212,7 @@ endmodule
 // Retrieval info: PRIVATE: PLL_PFDENA_CHECK STRING "0"
 // Retrieval info: PRIVATE: PLL_TARGET_HARCOPY_CHECK NUMERIC "0"
 // Retrieval info: PRIVATE: PRIMARY_CLK_COMBO STRING "inclk0"
-// Retrieval info: PRIVATE: RECONFIG_FILE STRING "sdram_clk_100MHz.mif"
+// Retrieval info: PRIVATE: RECONFIG_FILE STRING "clk_7seg.mif"
 // Retrieval info: PRIVATE: SACN_INPUTS_CHECK STRING "0"
 // Retrieval info: PRIVATE: SCAN_FEATURE_ENABLED STRING "1"
 // Retrieval info: PRIVATE: SELF_RESET_LOCK_LOSS STRING "0"
@@ -233,9 +233,9 @@ endmodule
 // Retrieval info: PRIVATE: ZERO_DELAY_RADIO STRING "0"
 // Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 // Retrieval info: CONSTANT: BANDWIDTH_TYPE STRING "AUTO"
-// Retrieval info: CONSTANT: CLK0_DIVIDE_BY NUMERIC "1"
+// Retrieval info: CONSTANT: CLK0_DIVIDE_BY NUMERIC "500"
 // Retrieval info: CONSTANT: CLK0_DUTY_CYCLE NUMERIC "50"
-// Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "2"
+// Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "1"
 // Retrieval info: CONSTANT: CLK0_PHASE_SHIFT STRING "0"
 // Retrieval info: CONSTANT: COMPENSATE_CLOCK STRING "CLK0"
 // Retrieval info: CONSTANT: INCLK0_INPUT_FREQUENCY NUMERIC "20000"
@@ -291,12 +291,12 @@ endmodule
 // Retrieval info: CONNECT: @inclk 0 0 1 1 GND 0 0 0 0
 // Retrieval info: CONNECT: @inclk 0 0 1 0 inclk0 0 0 0 0
 // Retrieval info: CONNECT: c0 0 0 0 0 @clk 0 0 1 0
-// Retrieval info: GEN_FILE: TYPE_NORMAL sdram_clk_100MHz.v TRUE
-// Retrieval info: GEN_FILE: TYPE_NORMAL sdram_clk_100MHz.ppf TRUE
-// Retrieval info: GEN_FILE: TYPE_NORMAL sdram_clk_100MHz.inc FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL sdram_clk_100MHz.cmp FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL sdram_clk_100MHz.bsf FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL sdram_clk_100MHz_inst.v FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL sdram_clk_100MHz_bb.v TRUE
+// Retrieval info: GEN_FILE: TYPE_NORMAL clk_7seg.v TRUE
+// Retrieval info: GEN_FILE: TYPE_NORMAL clk_7seg.ppf TRUE
+// Retrieval info: GEN_FILE: TYPE_NORMAL clk_7seg.inc FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL clk_7seg.cmp FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL clk_7seg.bsf FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL clk_7seg_inst.v FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL clk_7seg_bb.v TRUE
 // Retrieval info: LIB_FILE: altera_mf
 // Retrieval info: CBX_MODULE_PREFIX: ON
